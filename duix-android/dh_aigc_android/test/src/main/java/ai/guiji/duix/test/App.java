e059f67b9d63306457a9a618a9882efc7b8c5fe0
nhatanhd50-eng <nhatanhd50@gmail.com>
2026-01-31 09:42:48 +0700

    Update App.java

--- duix-android/dh_aigc_android/test/src/main/java/ai/guiji/duix/test/App.java
+++ duix-android/dh_aigc_android/test/src/main/java/ai/guiji/duix/test/App.java
@@ -1,10 +1,7 @@
 package ai.guiji.duix.test;
 
 import android.app.Application;
-import android.text.TextUtils;
-
 import java.util.concurrent.TimeUnit;
-
 import okhttp3.OkHttpClient;
 
 public class App extends Application {
@@ -21,9 +18,9 @@ public class App extends Application {
     public static OkHttpClient getOkHttpClient() {
         if (mOkHttpClient == null) {
             mOkHttpClient = new OkHttpClient.Builder()
-                    .connectTimeout(15, TimeUnit.SECONDS)
-                    .writeTimeout(15, TimeUnit.SECONDS)
-                    .readTimeout(15, TimeUnit.SECONDS)
+                    .connectTimeout(30, TimeUnit.SECONDS) // Tăng lên 30s
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
                     .build();
         }
         return mOkHttpClient;
